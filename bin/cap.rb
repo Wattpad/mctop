c833a7c9abc32c236f8d9ef2b2bbf6d678f59ea8
Paulo Bittencourt <paulo@wattpad.com>
2018-03-16 19:34:53 +0000

    Fix signal handling; Fix line parsing

--- bin/cap.rb
+++ bin/cap.rb
@@ -3,7 +3,7 @@ require 'thread'
 
 done = false
 
-%w[ INT QUIT HUP KILL ].each do |sig|
+%w[ INT QUIT HUP ].each do |sig|
   Signal.trap(sig) do
     puts "** Caught signal #{sig} - exiting"
     done = true
@@ -11,14 +11,16 @@ done = false
 end
 
 t = Thread.new {
-  cap = Pcap::Capture.open_live(@source, 1500)
+  cap = Pcap::Capture.open_live("eth0", 1500)
   cap.setfilter("port 11211")
   cap.loop do |packet|
     # parse key name, and size from VALUE responses
-    if packet.raw_data =~ /VALUE (\S+) \S+ (\S+)/
+    if packet.raw_data =~ /set (\S+) (\d+) (\d+) (\d+)\r\n/
       puts packet.raw_data
     end
     break if done
   end
   cap.close
 }
+
+t.join
